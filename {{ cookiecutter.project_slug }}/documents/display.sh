416d7dda02f5e900b59fcfce1f420523f3fa05e0
Mohammad Developer <MohammadDeveloperMail@gmail.com>
2023-03-10 22:56:34 +0330

    initial commit: ready up cookiecutterd3v

---
+++ {{ cookiecutter.project_slug }}/documents/display.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check distru for glow installation
+if ! [ $(command -v glow) == 0 ] then
+    if [ -d /etc/apt/ ] then
+        apt install glow -y
+    elif [ -f /etc/pacman.conf ] then
+        pacman -S glow --confirm
+    elif [ -f /etc/yum.conf ] then
+        yum install glow -y
+    fi
+
+# Run glow to show markdown files
+else
+    glow 
+fi
