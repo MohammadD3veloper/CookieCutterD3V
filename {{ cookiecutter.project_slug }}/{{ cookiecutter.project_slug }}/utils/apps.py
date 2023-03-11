0c44b6c78d4be335e5f4f531e5a2761bd6b3b0ad
Mohammad Developer <MohammadDeveloperMail@gmail.com>
2023-03-11 18:18:46 +0330

    fix django app names

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/utils/apps.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/utils/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UtilsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'utils'
+    name = '{{ cookiecutter.project_slug }}.utils'
